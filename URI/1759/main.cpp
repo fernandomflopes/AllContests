b0b29377654c429c4135dbd7348b062fb1a9a22b
Fernando MF Lopes <contato.fernandomflopes@gmail.com>
2022-07-16 14:14:05 -0300

    1759

---
+++ URI/1759/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int ct;
+
+    cin >> ct;
+
+    while(ct > 1) {
+
+        cout << "Ho" << " ";
+
+        ct--;
+    }
+
+    cout << "Ho!" << endl;
+
+    return 0;
+}
