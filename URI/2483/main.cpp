f69dc0769cd82afcf02382680174ed992d291e66
Fernando MF Lopes <contato.fernandomflopes@gmail.com>
2022-07-17 14:49:49 -0300

    2483

---
+++ URI/2483/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    cout << "Feliz nat";
+    while(n > 0) {
+        cout << "a";
+        n--;
+    }
+    cout << "l!" << endl;
+
+
+    return 0;
+}
