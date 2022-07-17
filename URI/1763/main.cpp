df49dfba0ac951bc853be78ecd347f02b4b828b2
Fernando MF Lopes <contato.fernandomflopes@gmail.com>
2022-07-17 15:37:20 -0300

    1763

---
+++ URI/1763/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    string pais;
+    map<string, string> m;
+
+    m["brasil"] = "Feliz Natal!";
+    m["alemanha"] = "Frohliche Weihnachten!";
+    m["austria"] = "Frohe Weihnacht!";
+    m["coreia"] = "Chuk Sung Tan!";
+    m["espanha"] = "Feliz Navidad!";
+    m["grecia"] = "Kala Christougena!";
+    m["estados-unidos"] = "Merry Christmas!";
+    m["inglaterra"] = "Merry Christmas!";
+    m["australia"] = "Merry Christmas!";
+    m["portugal"] = "Feliz Natal!";
+    m["suecia"] = "God Jul!";
+    m["turquia"] = "Mutlu Noeller";
+    m["argentina"] = "Feliz Navidad!";
+    m["chile"] = "Feliz Navidad!";
+    m["mexico"] = "Feliz Navidad!";
+    m["antardida"] = "Merry Christmas!";
+    m["canada"] = "Merry Christmas!";
+    m["irlanda"] = "Nollaig Shona Dhuit!";
+    m["belgica"] = "Zalig Kerstfeest!";
+    m["italia"] = "Buon Natale!";
+    m["libia"] = "Buon Natale!";
+    m["siria"] = "Milad Mubarak!";
+    m["marrocos"] = "Milad Mubarak!";
+    m["japao"] = "Merii Kurisumasu!";
+
+
+    while(!cin.eof()) {
+        cin >> pais;
+
+        if(!m.count(pais)) {
+            cout << "--- NOT FOUND ---" << endl;
+        } else {
+            cout << m[pais] << endl;
+        }
+    }
+
+    return 0;
+}
